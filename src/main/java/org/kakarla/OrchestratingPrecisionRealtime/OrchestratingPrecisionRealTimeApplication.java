8f300b03daff84b6842c5cd0ef503d99a6baffff
tarun <kakarlatarun72@gmail.com>
2024-01-05 01:47:18 -0500

    intial commit

---
+++ src/main/java/org/kakarla/OrchestratingPrecisionRealtime/OrchestratingPrecisionRealTimeApplication.java
@@ -0,0 +1,13 @@
+package org.kakarla.OrchestratingPrecisionRealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratingPrecisionRealTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratingPrecisionRealTimeApplication.class, args);
+	}
+
+}
